5701577788e1e3b5257d74c9964685d73c33d519
Gr1mMsh@rp <92096482+grimmsharpXataro@users.noreply.github.com>
2021-10-08 10:44:32 +0200

    Update no2

--- binscchub-bot/grim.py
+++ binscchub-bot/grim.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
-
+import time
 
 URL = 'https://binscchub.com/'
 LOGFILE = "log.txt"
 
 browser = webdriver.Firefox()
-browser.get(url)
+browser.get(URL)
 
 time.sleep(1)
 
@@ -13,7 +13,9 @@ for i in range(300, 399):
 
 	try:
 
-		element=browser.find_element_by_id('post-{}'.format(str(i)))
+		print("|*| Trying post-{}".format(str(i)))
+
+		element = browser.find_element_by_id('post-{}'.format(str(i)))
 		element.click()
 
 		bins = browser.find_element_by_class('entry-content')
@@ -26,10 +28,16 @@ for i in range(300, 399):
 			fd = open(LOGFILE, "a").write(bin_content)
 			fclose(fd)
 
+			print("|+| OK")
+
 		except Exception as E:
 
 			print("|-| An exception occured : " + str(E))
 
-	except: pass
+	except: print("|-| Not found")	
+
+	time.sleep(1)
+
+
 
 
