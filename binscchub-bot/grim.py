f30a85dcbeb0d5e176613f6959c9f5f7582d97f2
Gr1mMsh@rp <92096482+grimmsharpXataro@users.noreply.github.com>
2021-10-08 10:01:12 +0200

    Bins CC hub bot project

---
+++ binscchub-bot/grim.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+
+
+URL = 'https://binscchub.com/'
+
+browser = webdriver.Chrome()
+browser.get(url)
+
+time.sleep(1)
+
+for i in range(300, 399):
+
+	try:
+
+		element=browser.find_element_by_id('post-{}'.format(str(i)))
+		element.click()
+
+		bins = browser.find_element_by_class('entry-content')
+		bin_content = bins.text 
+
+	except: pass
+
+
