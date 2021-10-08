068e81c3bd177bca35e16fc78503ee7745180b95
ATAR <65398082+xatarox@users.noreply.github.com>
2021-10-08 10:29:33 +0200

    Add files via upload

---
+++ binscchub-bot/atar.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+import time
+
+
+url = 'https://binscchub.com/'
+
+browser = webdriver.Chrome()
+browser.get(url)
+
+time.sleep(1)
+
+for i in range(300, 399):
+
+	try:
+
+		element=browser.find_element_by_id('post-{}'.format(str(i)))
+		element.click()
+
+		bins = browser.find_element_by_class('entry-content')
+		bin_content = bins.text 
+        
+	except: pass
+with open('output.txt', 'w') as f:
+    f.write(bin_content.text)
